47883273f80069d03c0c800b55b73bcec290785d
PhanDuc2k3 <21011596@st.phenikaa-uni.edu.vn>
2025-10-30 23:10:04 +0700

    fix

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Banner from "../../components/Home/Banner/Banner";
 import Categories from "../../components/Home/Categories/Categories";
-import FeaturedProduct from "../../components/Store/FeaturedProduct/FeaturedProduct";
+import FeaturedProducts from "../../components/Home/FeaturedProducts/FeaturedProducts";
 import FeaturedStores from "../../components/Home/FeaturedStores/FeaturedStores";
 import SellerBox from "../../components/Home/SellerBox/SellerBox";
 
 const Home: React.FC = () => {
-  const storeId = "default_store_id"; // 👈 luôn truyền storeId
-
   return (
     <>
       <Banner />
       <Categories />
-      <FeaturedProduct storeId={storeId} />
+      <FeaturedProducts />
       <FeaturedStores />
       <SellerBox />
     </>
